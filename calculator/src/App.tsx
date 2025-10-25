1b363b664b908ae4c792fecd4faee11e699ff778
mihail <malitskiy.mikhail@gmail.com>
2025-10-25 22:10:53 +0200

    file: add files

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Greet} from './components/Greet'
-
+import { Greet } from './components/Greet'
+import { Person } from './components/Person'
 
 const App: React.FC = () => {
-  return (
+
+    return (
       <div>
-        <Greet name="10" />
+        <Greet name="Vishwas" messageCount={20} isLoggedIn={false}/>
       </div>
   );
 };
